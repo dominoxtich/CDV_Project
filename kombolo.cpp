a3d9073528091a38d2b1fb42075575b1be7ffc06
dominoxtich <dpochylski@edu.cdv.pl>
2024-12-23 10:44:36 +0100

    first file

---
+++ kombolo.cpp
@@ -0,0 +1,15 @@
+//
+// Created by d.pochylski on 23.12.2024.
+//
+#include <iostream>
+using namespace std;
+int main() {
+  int i = 0;
+  for(i = 1;i<10;i++){
+           cout << i << ": lol"+i << endl;
+          }
+       return 0;
+
+
+
+}
